3dfef5a8e147a3bb1007d3d5c9494e71a499940e
Ryan Wei <rainmanwy@gmail.com>
2025-12-04 12:07:33 +0800

    Improve execution logic

--- 01_gen_data/rollout.sh
+++ 01_gen_data/rollout.sh
@@ -179,6 +179,15 @@ function gen_data() {
   fi
 }
 
+function copy_tpc()
+{
+  cp ${TPC_H_DIR}/00_compile_tpch/dbgen/dbgen ${TPC_H_DIR}/*_gen_data/
+  cp ${TPC_H_DIR}/00_compile_tpch/dbgen/dists.dss ${TPC_H_DIR}/*_gen_data/
+}
+
+
+${TPC_H_DIR}/00_compile_tpch/dbgen/dbgen ${TPC_H_DIR}/00_compile_tpch/dbgen/dists.dss
+
 step="gen_data"
 
 log_time "Step ${step} started"
@@ -187,6 +196,7 @@ init_log ${step}
 start_log
 schema_name=${DB_VERSION}
 table_name="gen_data"
+copy_tpc
 
 if [ "${GEN_NEW_DATA}" == "true" ]; then
   log_time "Start generating data with RUN_MODEL ${RUN_MODEL} with GEN_DATA_SCALE ${GEN_DATA_SCALE}."
